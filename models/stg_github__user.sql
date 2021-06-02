f696d2131b18d280f45b6fda86d25d34646cae4a
Tom Nagengast <tnagengast@gmail.com>
2021-06-02 16:22:00 -0700

    Revert back to inital formatting to keep parity with forked repo

--- models/stg_github__user.sql
+++ models/stg_github__user.sql
@@ -1,15 +1,10 @@
 {{ config(alias='stg_github_user') }}
+with github_user as (
 
-with
+    select *
+    from {{ var('user') }}
 
-github_user as (
-
-    select * from {{ var('user') }}
-
-),
-
-macro as (
-
+), macro as (
     select
         /*
         The below macro is used to generate the correct SQL for package staging models. It takes a list of columns
@@ -18,31 +13,34 @@ macro as (
 
         For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
         */
-        {{ fivetran_utils.fill_staging_columns(
-            source_columns=adapter.get_columns_in_relation(var('user')),
-            staging_columns=get_user_columns()
-        ) }}
+            {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(var('user')),
+                staging_columns=get_user_columns()
+            )
+        }}
+
+
     from github_user
 
-),
-
-fields as (
+), fields as (
 
     select
-        id as user_id,
-        login as login_name,
-        "TYPE" as user_type,
-        site_admin,
-        name as user_name,
-        company,
-        blog,
-        location,
-        hireable,
-        bio,
-        created_at,
-        updated_at
-    from macro
+      id as user_id,
+      login as login_name,
+      name,
+      company
+      type,
+      site_admin,
+      blog,
+      location,
+      hireable,
+      bio,
+      created_at,
+      updated_at
 
+    from macro
 )
 
-select * from fields
+select *
+from fields
